cec181ec7df6fb61c5a137cbe456b274b23aa016
Vamsi <vamsik384@gmail.com>
2018-05-09 11:42:58 +0530

    first commit by susmitha

---
+++ src/test/java/abc/AbcTest.java
@@ -0,0 +1,19 @@
+package abc;
+
+import org.testng.annotations.Test;
+
+public class AbcTest 
+{
+
+	@Test
+	public void m1()
+	{
+		System.out.println("Hii this is m1 methods");
+	}
+	
+	@Test
+	public void m2()
+	{
+		System.out.println("Hii this is m2 method");
+	}
+}
